ba099061d2d2e27f1414176c03083c0ddd4cfbc3
Avery Little <avery.little@industrystar.com>
2020-12-02 22:50:27 -0500

    added nodemon and some REST endpoints

--- index.js
+++ index.js
@@ -24,12 +24,45 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) =>{
+// Using express means we don't have to use stringify
+app.get('/', (request, response) => {
     response.send('<h1>Hello</h1>')
 })
 
+// Get all endpoint
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    response.json(persons)
+})
+
+// Get 1 person endpoint
+// Parameters come in as strings and need to be checked for correct format :)
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+// Delete 1 person endpoint
+// Tested with postman
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+// Info page
+app.get('/info', (request, response) => {
+    let entries = persons.length
+    let date = new Date()
+    response.send(`<p>Phonebook has info for ${entries} people</p>
+                    <p>${date}</p>`)
 })
 
 const PORT = 3001
